f04fca59d99e4a3339b3d6104257be371da870ab
M Vendors <m-vendors@protonmail.com>
2021-05-23 21:12:06 +0300

    Initial Commit

---
+++ registration.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'zenda_shipping_DummyPkg',
+    __DIR__
+);
